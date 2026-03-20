a6366db8604df6e471ef0c41a982c99c9b376d9a
fbkl <frkle@hvl.no>
2026-03-20 09:11:39 +0100

    .

---
+++ acquisition_fb_flexbe_behaviors/src/acquisition_fb_flexbe_behaviors/bringup_vio_sm.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+###########################################################
+#               WARNING: Generated code!                  #
+#              **************************                 #
+# Manual changes may get lost if file is generated again. #
+# Only code inside the [MANUAL] tags will be kept.        #
+###########################################################
+
+from flexbe_core import Behavior, Autonomy, OperatableStateMachine, ConcurrencyContainer, PriorityContainer, Logger
+from acquisition_fb_flexbe_states.tmux_setup_state import TmuxSetupState
+# Additional imports can be added inside the following tags
+# [MANUAL_IMPORT]
+
+# [/MANUAL_IMPORT]
+
+
+'''
+Created on Thu Mar 19 2026
+@author: fbk
+'''
+class bringup_vioSM(Behavior):
+	'''
+	trying to get vio running remotely,, 
+	'''
+
+
+	def __init__(self):
+		super(bringup_vioSM, self).__init__()
+		self.name = 'bringup_vio'
+
+		# parameters of this behavior
+
+		# references to used behaviors
+
+		# Additional initialization code can be added inside the following tags
+		# [MANUAL_INIT]
+		
+		# [/MANUAL_INIT]
+
+		# Behavior comments:
+
+
+
+	def create(self):
+		# x:30 y:516, x:130 y:516
+		_state_machine = OperatableStateMachine(outcomes=['finished', 'failed'])
+
+		# Additional creation code can be added inside the following tags
+		# [MANUAL_CREATE]
+		
+		# [/MANUAL_CREATE]
+
+
+		with _state_machine:
+			# x:231 y:109
+			OperatableStateMachine.add('past_frederico_is_a_magic_man',
+										TmuxSetupState(session_name=testtt, startup_dic={"camera1":["roslaunch jetpack a_camera.launch cam:=cam1"]}),
+										transitions={'continue': 'finished', 'failed': 'failed'},
+										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off})
+
+
+		return _state_machine
+
+
+	# Private functions can be added inside the following tags
+	# [MANUAL_FUNC]
+	
+	# [/MANUAL_FUNC]
